e36c12db65191bb4b781c0b5e9410b46eee2851f
B-Tron510 <bryanfmarks@gmail.com>
2015-10-28 19:11:07 -0700

    Setting up a starter Ways Server

---
+++ textapp/README.md
@@ -0,0 +1,5 @@
+# textapp
+
+a [Sails](http://sailsjs.org) application
+
+1. run sails lift.
---
+++ textapp/api/controllers/WaysController.js
@@ -0,0 +1,11 @@
+/**
+ * WaysController
+ *
+ * @description :: Server-side logic for managing ways
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
